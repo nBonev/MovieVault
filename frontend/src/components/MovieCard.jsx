2c7a8bd103db591bbb449050d28112d78de1390a
Nikolay <nbonev5@gmail.com>
2025-03-19 16:32:49 +0200

    Add and remove favorites movies

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,9 +1,14 @@
 import "../css/MovieCard.css"
+import { useMovieContext } from "../contexts/MovieContext"
 
 export default function MovieCard({movie}) {
+    const {isFavorite, addToFavorites, removeFromFavorites} = useMovieContext();
+    const favorite = isFavorite(movie.id);
 
-    const onFavoriteClick = () => {
-        alert('Clicked!')
+    const onFavoriteClick = (e) => {
+        e.preventDefault();
+        if (favorite) removeFromFavorites(movie.id)
+        else addToFavorites(movie)
     }
 
     return (
@@ -11,7 +16,7 @@ export default function MovieCard({movie}) {
             <div className="movie-poster">
                 <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
                 <div className="movie-overlay">
-                    <button className="favorite-btn" onClick={onFavoriteClick}>♥</button>
+                    <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>♥</button>
                 </div>
             </div>
             <div className="movie-info">
