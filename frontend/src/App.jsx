ccf68983cdcdbe5c4ae2cbd5f4a7ac0ae4618e30
Nikolay <nbonev5@gmail.com>
2025-03-19 14:30:24 +0200

    Add navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
+import NavBar from './components/NavBar';
 import Favorites from './pages/Favorites';
 import Home from './pages/Home';
 import {Routes, Route} from 'react-router-dom'
 
 function App() {
-    const movieNumber = 1;
 
   return (
+    <div>
+      <NavBar />
     <main className='main-content'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favorites' element={<Favorites />} />
       </Routes>
     </main>
+    </div>
   )
 }
 
