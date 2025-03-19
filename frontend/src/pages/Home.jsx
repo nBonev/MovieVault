dbf9c6f62f73c0f94d92c5d72feb55566eb231e0
Nikolay <nbonev5@gmail.com>
2025-03-19 17:03:17 +0200

    Change the background-color

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,6 +35,7 @@ export default function Home() {
 
         try {
             const searchResults = await searchMovies(searchQuery);
+            
             setMovies(searchResults)
             setError(null)
         } catch (err) {
